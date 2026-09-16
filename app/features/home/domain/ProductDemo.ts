a4c5f5100c1b9eb1e996fedf93239e0b67d79c62
rouzbeh <95648052+rouzbehtondkar@users.noreply.github.com>
2026-09-16 18:12:58 +0330

    feat: add Product OOP demo objects

---
+++ app/features/home/domain/ProductDemo.ts
@@ -0,0 +1,7 @@
+import { Product } from "./Product";
+
+export const demoProducts = [
+  new Product("هدفون آوا", "صوت و تصویر", 3890000),
+  new Product("ساعت نوا 02", "پوشیدنی هوشمند", 5450000),
+  new Product("چراغ رومیزی لاین", "خانه و دکور", 1790000),
+];
