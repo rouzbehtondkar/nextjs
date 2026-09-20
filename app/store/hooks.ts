df80b9d30c058f3de6d3dff0c983f6f9b4cb74f7
rouzbeh <95648052+rouzbehtondkar@users.noreply.github.com>
2026-09-20 19:16:45 +0330

    docs: add Persian learning comments to app/store/hooks.ts

--- app/store/hooks.ts
+++ app/store/hooks.ts
@@ -1,5 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "./store";
 
+// نسخه TypeScript-friendly از useDispatch برای ارسال Actionها.
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
+// نسخه TypeScript-friendly از useSelector برای خواندن اطلاعات Store.
 export const useAppSelector = useSelector.withTypes<RootState>();
